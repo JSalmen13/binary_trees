25005252e22955b285fe997534dc4ebe49d27910
Jsalmen13 <jarraya.salmen13@gmail.com>
2021-03-12 14:55:22 +0000

    9th fix'

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,11 +7,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-
-	if (binary_tree_height(tree->left) > binary_tree_height(tree->right))
-		return (binary_tree_height(tree->left) + 1);
-	else
-		return (binary_tree_height(tree->right) + 1);
+    int Lcount = 0;
+    int Rcount = 0;
+    
+    if (!tree)
+        return (0);
+    if (tree->left)
+        Lcount = 1 + binary_tree_height(tree->left);
+    if (tree->right)
+        Rcount = 1 + binary_tree_height(tree->right);
+    if (Lcount > Rcount)
+        return (Lcount);
+    else
+        return (Rcount);
 }
