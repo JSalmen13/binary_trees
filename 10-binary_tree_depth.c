538292ed9cb8b6d927d7adc3be826b3fc5cf067e
Jsalmen13 <jarraya.salmen13@gmail.com>
2021-03-12 14:59:32 +0000

    10th

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth -depth of tree
+ *
+ * @tree: root node
+ * Return: depth of the tree
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t dpth = 0;
+
+	if (tree)
+	{
+		for(;tree->parent;dpth++)
+			tree = tree->parent;
+	}
+	return (dpth);
+}
