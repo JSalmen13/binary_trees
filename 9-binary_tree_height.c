88b4f9d172033419b2bc94e0814694899605351e
Jsalmen13 <jarraya.salmen13@gmail.com>
2021-03-12 14:56:58 +0000

    9th fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,17 +7,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    int Lcount = 0;
-    int Rcount = 0;
-    
-    if (!tree)
-        return (0);
-    if (tree->left)
-        Lcount = 1 + binary_tree_height(tree->left);
-    if (tree->right)
-        Rcount = 1 + binary_tree_height(tree->right);
-    if (Lcount > Rcount)
-        return (Lcount);
-    else
-        return (Rcount);
+	int Lcount = 0;
+	int Rcount = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		Lcount = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		Rcount = 1 + binary_tree_height(tree->right);
+	if (Lcount > Rcount)
+		return (Lcount);
+	else
+		return (Rcount);
 }
